54d10d605b046e44c192650525cc30cb53d19dad
Ju Liu <ju.liu@welaika.com>
2014-10-21 21:19:07 +0100

    Set shell name to app name in Rails apps

--- lib/pry-bloodline/railtie.rb
+++ lib/pry-bloodline/railtie.rb
@@ -2,6 +2,9 @@ module PryBloodline
   class Railtie < Rails::Railtie
     initializer 'pry-bloodline.initialize' do |app|
       PryBloodline.setup!
+      PryBloodline.configure do |c|
+        c.name = app.class.parent_name.underscore
+      end
     end
   end
 end
