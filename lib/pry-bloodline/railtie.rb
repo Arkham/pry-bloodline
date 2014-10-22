5ba0580e99c6c882ac826ea2bf627880b65c8ad4
Abdelkader Boudih <terminale@gmail.com>
2014-10-22 14:50:14 +0000

    Tell don't ask

--- lib/pry-bloodline/railtie.rb
+++ lib/pry-bloodline/railtie.rb
@@ -1,9 +1,14 @@
-module PryBloodline
-  class Railtie < Rails::Railtie
-    initializer 'pry-bloodline.initialize' do |app|
-      PryBloodline.setup!
-      PryBloodline.configure do |c|
-        c.name = app.class.parent_name.underscore
+begin
+  require 'rails/railtie'
+rescue LoadError
+else
+  module PryBloodline
+    class Railtie < Rails::Railtie
+      initializer 'pry-bloodline.initialize' do |app|
+        PryBloodline.setup!
+        PryBloodline.configure do |c|
+          c.name = app.class.parent_name.underscore
+        end
       end
     end
   end
